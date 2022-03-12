5895d3537627955ec5896798d4fb743f8609a5dd
Ismael Antonio Gomez Perez <89043543+IsmaelGomezPerez@users.noreply.github.com>
2022-03-11 23:40:57 -0600

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('#bmenu').click(function(){
+    $('#blinks').slideToggle();
+    })
+    $('#btn-flecha1').click(function(){
+        $('#textoPre').slideToggle();
+        $('#abajo').toggle();
+        $('#arriba').toggle();
+        })
+    $('#btn-flecha2').click(function(){
+        $('#textoPre2').slideToggle();
+        $('#abajo2').toggle();
+        $('#arriba2').toggle();
+        })
+    $('#btn-flecha3').click(function(){
+        $('#textoPre3').slideToggle();
+        $('#abajo3').toggle();
+        $('#arriba3').toggle();
+        })   
+    $('#btn-flecha4').click(function(){
+        $('#textoPre4').slideToggle();
+        $('#abajo4').toggle();
+        $('#arriba4').toggle();
+        })   
+    $('#btn-flecha5').click(function(){
+        $('#textoPre5').slideToggle();
+        $('#abajo5').toggle();
+        $('#arriba5').toggle();
+        })           
+
+})
